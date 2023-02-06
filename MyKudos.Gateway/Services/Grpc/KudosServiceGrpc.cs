c8b0fb31fb16ff69fe52377186266c0d6bcb80a0
Luis Henrique Demetrio <luisdem@microsoft.com>
2023-02-06 20:54:06 -0300

    fixed un matching return type of 'string' for the  'IKudosService.Send(KudosRequest)'

--- MyKudos.Gateway/Services/Grpc/KudosServiceGrpc.cs
+++ MyKudos.Gateway/Services/Grpc/KudosServiceGrpc.cs
@@ -1,86 +1,86 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using Grpc.Net.Client;
-using MyKudos.Gateway.Interfaces;
-using MyKudos.Gateway.Models;
-using MyKudos.Kudos.gRPC;
-using static MyKudos.Kudos.gRPC.KudosService;
+﻿//using Google.Protobuf.WellKnownTypes;
+//using Grpc.Net.Client;
+//using MyKudos.Gateway.Interfaces;
+//using MyKudos.Gateway.Models;
+//using MyKudos.Kudos.gRPC;
+//using static MyKudos.Kudos.gRPC.KudosService;
 
-namespace MyKudos.Gateway.Services;
+//namespace MyKudos.Gateway.Services;
 
-public class KudosServiceGrpc : IKudosService
-{
+//public class KudosServiceGrpc : IKudosService
+//{
 
-    private readonly string _kudosServiceUrl;
+//    private readonly string _kudosServiceUrl;
 
-    public KudosServiceGrpc(IConfiguration config)
-    {
-        _kudosServiceUrl = config["kudosServiceUrl"];
-    }
+//    public KudosServiceGrpc(IConfiguration config)
+//    {
+//        _kudosServiceUrl = config["kudosServiceUrl"];
+//    }
 
-    public IEnumerable<Models.Kudos> GetKudos()
-    {
+//    public IEnumerable<Models.Kudos> GetKudos()
+//    {
 
-        List<Models.Kudos> result = new();
+//        List<Models.Kudos> result = new();
 
-        var client = new KudosServiceClient(
-                            GrpcChannel.ForAddress(_kudosServiceUrl)
-                         );
+//        var client = new KudosServiceClient(
+//                            GrpcChannel.ForAddress(_kudosServiceUrl)
+//                         );
 
-        var kudos = client.GetKudos(new Empty());
+//        var kudos = client.GetKudos(new Empty());
 
-        foreach (var item in kudos.Data)
-        {
-            result.Add(new Models.Kudos() {
-                Id = item.Id,
-                FromPersonId = item.FromPersonId,
-                ToPersonId = item.ToPersonId,
-                TitleId = item.TitleId,
-                Message = item.Message,
-                Date = item.Date.ToDateTime(),
-                Likes = item.Likes.Id.Select(x => x.Id).ToList()
-                }) ;
-        }
-
-
-        return result;
+//        foreach (var item in kudos.Data)
+//        {
+//            result.Add(new Models.Kudos() {
+//                Id = item.Id,
+//                FromPersonId = item.FromPersonId,
+//                ToPersonId = item.ToPersonId,
+//                TitleId = item.TitleId,
+//                Message = item.Message,
+//                Date = item.Date.ToDateTime(),
+//                Likes = item.Likes.Id.Select(x => x.Id).ToList()
+//                }) ;
+//        }
 
 
-    }
+//        return result;
 
-    public bool Send(Models.KudosRequest kudos)
-    {
+
+//    }
+
+//    public bool Send(Models.KudosRequest kudos)
+//    {
         
-        var client = new KudosServiceClient(
-                            GrpcChannel.ForAddress(_kudosServiceUrl)
-                         );
+//        var client = new KudosServiceClient(
+//                            GrpcChannel.ForAddress(_kudosServiceUrl)
+//                         );
 
-        var r = client.SendKudos(new Kudos.gRPC.KudosRequest()
-        {
-            FromPersonId = kudos.From.Id,
-            ToPersonId = kudos.To.Id,
-            Message = kudos.Message,
-            TitleId = kudos.Title.Id,
-            SendOn = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc).ToTimestamp()
-        }) ;
+//        var r = client.SendKudos(new Kudos.gRPC.KudosRequest()
+//        {
+//            FromPersonId = kudos.From.Id,
+//            ToPersonId = kudos.To.Id,
+//            Message = kudos.Message,
+//            TitleId = kudos.Title.Id,
+//            SendOn = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc).ToTimestamp()
+//        }) ;
 
-        return r.Succeed;
-    }
+//        return r.Succeed;
+//    }
 
-    public bool SendLike(LikeGateway like)
-    {
-        List<Models.Kudos> result = new();
+//    public bool SendLike(LikeGateway like)
+//    {
+//        List<Models.Kudos> result = new();
 
-        var client = new KudosServiceClient(
-                            GrpcChannel.ForAddress(_kudosServiceUrl)
-                         );
+//        var client = new KudosServiceClient(
+//                            GrpcChannel.ForAddress(_kudosServiceUrl)
+//                         );
 
-        var r = client.SendLike(new Kudos.gRPC.SendLikeRequest()
-        {
-            KudosId = like.KudosId,
-            PersonId = like.PersonId
-        });
+//        var r = client.SendLike(new Kudos.gRPC.SendLikeRequest()
+//        {
+//            KudosId = like.KudosId,
+//            PersonId = like.PersonId
+//        });
 
-        return r.Succeed;
+//        return r.Succeed;
 
-    }
-}
+//    }
+//}
