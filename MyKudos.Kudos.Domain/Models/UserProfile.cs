0f07e91a80174ca8c44b7592b3f9f1d84d862e50
Luis Henrique Demetrio <luisdem@microsoft.com>
2023-06-15 17:57:43 -0300

    [MaxLength(60)] for userprofile displayname

--- MyKudos.Kudos.Domain/Models/UserProfile.cs
+++ MyKudos.Kudos.Domain/Models/UserProfile.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace MyKudos.Kudos.Domain.Models;
 
 public class UserProfile
 {
     public Guid UserProfileId { get; set; }
+    [MaxLength(60)]
     public string DisplayName { get; set; }
     
     public string? Photo { get; set; }
