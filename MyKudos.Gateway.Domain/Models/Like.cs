42fab374bb5dc232e9d0134719f1e130142aa390
Luis Henrique Demetrio <luisdem@microsoft.com>
2023-09-06 18:05:26 -0300

    added the kudos item razor component that will handle the logic for edit and delete kudos

--- MyKudos.Gateway.Domain/Models/Like.cs
+++ MyKudos.Gateway.Domain/Models/Like.cs
@@ -3,6 +3,8 @@
 
 public record LikeGateway(int KudosId, Person FromPerson);
 
+public record SendLikeGateway(int KudosId, Guid UserProfileId);
+
 
 public record LikeCommentGateway(int KudosId, Person FromPerson);
 
