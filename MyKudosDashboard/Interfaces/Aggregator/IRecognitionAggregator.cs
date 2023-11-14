9b05c4d11451de44c867d958ca4571a505328be0
Luis Henrique Demetrio <luisdem@microsoft.com>
2023-11-14 14:27:33 -0300

    initial

---
+++ MyKudosDashboard/Interfaces/Aggregator/IRecognitionAggregator.cs
@@ -0,0 +1,14 @@
+﻿using MyKudos.Gateway.Domain.Models;
+
+namespace MyKudosDashboard.Interfaces.Aggregator;
+
+public interface IRecognitionAggregator
+{
+    public Task<IEnumerable<Recognition>> GetRecognitions();
+
+    public Task<bool> AddNewRecognition(Recognition group);
+
+    Task<bool> DeleteRecognition(int recognitionId);
+
+    Task<bool> UpdateRecognition(Recognition group);
+}
